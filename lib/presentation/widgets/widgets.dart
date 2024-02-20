7024643e3da7e88d617c62d64a6a2d5ef71f7eba
Luis Salcido <salcidogrijalva@outlook.com>
2024-02-20 16:25:36 -0700

    Full Screen Loader added

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,4 +1,5 @@
+export 'movies/movies_horizontal_listview.dart';
+export 'movies/movies_slideshow.dart';
 export 'shared/custom_appbar.dart';
 export 'shared/custom_bottom_navigation.dart';
-export 'movies/movies_slideshow.dart';
-export 'movies/movies_horizontal_listview.dart';
+export 'shared/full_screen_loader.dart';
