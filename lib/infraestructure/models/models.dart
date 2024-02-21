24ce342d8d3ea54c46b864aa4b3a40ceb26dcb78
Luis Salcido <salcidogrijalva@outlook.com>
2024-02-21 14:33:56 -0700

    Movie Details model added

---
+++ lib/infraestructure/models/models.dart
@@ -0,0 +1,3 @@
+export 'package:cinemapedia/infraestructure/models/moviedb/movie_details.dart';
+export 'package:cinemapedia/infraestructure/models/moviedb/movie_from_moviedb.dart';
+export 'package:cinemapedia/infraestructure/models/moviedb/moviedb_response.dart';
