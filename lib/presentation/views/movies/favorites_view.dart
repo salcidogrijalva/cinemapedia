f24286904dbaadc967718ab04cf6ec487a98e64c
Luis Salcido <salcidogrijalva@outlook.com>
2024-02-28 16:56:18 -0700

    Temporary ListView to display our favorite movies

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,16 +1,34 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
-      ),
-      body: const Center(
-        child: Text('Favorites'),
+      body: ListView.builder(
+        itemCount: favoriteMovies.length,
+        itemBuilder: (context, index) {
+          final favoriteMovie = favoriteMovies[index];
+          return ListTile(
+            title: Text(favoriteMovie.title),
+          );
+        },
       ),
     );
   }
