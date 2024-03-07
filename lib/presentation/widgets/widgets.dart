4157a1442214a18ebd7da85d7558860a7193ee9c
Luis Salcido <salcidogrijalva@outlook.com>
2024-03-07 15:02:28 -0700

    MovieMasonry and MoviePosterLink widgets added

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,5 +1,7 @@
-export 'movies/movies_horizontal_listview.dart';
+export 'movies/movie_masonry.dart';
+export 'movies/movie_poster_link.dart';
 export 'movies/movie_rating_indicator.dart';
+export 'movies/movies_horizontal_listview.dart';
 export 'movies/movies_slideshow.dart';
 export 'shared/custom_appbar.dart';
 export 'shared/custom_bottom_navigation.dart';
